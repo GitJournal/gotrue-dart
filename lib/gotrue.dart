f26ba82104bace9f578ecce08dca5d151d5a5cbd
Vishesh Handa <me@vhanda.in>
2020-10-31 22:28:42 +0100

    Split settings into their own file

--- lib/gotrue.dart
+++ lib/gotrue.dart
@@ -3,3 +3,4 @@ library gotrue;
 export 'src/admin.dart';
 export 'src/user.dart';
 export 'src/gotrue.dart';
+export 'src/settings.dart';
--- lib/src/gotrue.dart
+++ lib/src/gotrue.dart
@@ -1,6 +1,6 @@
 import 'package:gotrue/gotrue.dart';
+import 'package:gotrue/src/settings.dart';
 import 'package:gotrue/src/token.dart';
-import 'package:meta/meta.dart';
 import 'package:dio/dio.dart';
 
 class GoTrue {
@@ -36,84 +36,6 @@ class GoTrue {
   Dio _dio() {
     return Dio(BaseOptions(baseUrl: apiUrl));
   }
-}
-
-class GoTrueSettings {
-  final GoTrueExternalSettings extern;
-  final bool disableSignup;
-  final bool autoconfirm;
-
-  GoTrueSettings({
-    @required this.extern,
-    @required this.disableSignup,
-    @required this.autoconfirm,
-  });
-
-  static GoTrueSettings fromJson(Map<String, dynamic> map) {
-    return GoTrueSettings(
-      autoconfirm: map['autoconfirm'],
-      disableSignup: map['disable_signup'],
-      extern: GoTrueExternalSettings.fromJson(map['external']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'external': extern,
-      'autoconfirm': autoconfirm,
-      'disable_signup': disableSignup,
-    };
-  }
-
-  @override
-  String toString() => toJson().toString();
-}
-
-class GoTrueExternalSettings {
-  final bool bitbucket;
-  final bool github;
-  final bool gitlab;
-  final bool google;
-  final bool facebook;
-  final bool email;
-  final bool saml;
-
-  GoTrueExternalSettings({
-    @required this.bitbucket,
-    @required this.github,
-    @required this.gitlab,
-    @required this.google,
-    @required this.facebook,
-    @required this.email,
-    @required this.saml,
-  });
-
-  static GoTrueExternalSettings fromJson(Map<String, dynamic> map) {
-    return GoTrueExternalSettings(
-      bitbucket: map['bitbucket'],
-      github: map['github'],
-      gitlab: map['gitlab'],
-      google: map['google'],
-      facebook: map['facebook'],
-      email: map['email'],
-      saml: map['saml'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'bitbucket': bitbucket,
-      'github': github,
-      'gitlab': gitlab,
-      'google': google,
-      'facebook': facebook,
-      'email': email,
-      'saml': saml,
-    };
-  }
-
-  @override
-  String toString() => toJson().toString();
 }
 
 // API URL
